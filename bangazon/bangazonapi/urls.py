656c654521f4e3c1d9e7364a99a64067a0966ad1
Nathan T. Baker <nathantbaker@gmail.com>
2017-01-24 15:53:19 -0600

    route urls for products and products-list

--- bangazon/bangazonapi/urls.py
+++ bangazon/bangazonapi/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
+from bangazonapi import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('bangazonapi.urls')),
+    url(r'^$', Products_List.as_view(), name='products'),
+    url(r'^$', Products.as_view(), name='products/details'),
 ]
