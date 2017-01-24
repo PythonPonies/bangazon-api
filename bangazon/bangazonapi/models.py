3cef570692e07933243f5b4f6a5e95268c822b31
ZoeLeBlanc <zoe.leblanc@vanderbilt.edu>
2017-01-24 11:13:34 -0600

    up to date

--- bangazon/bangazonapi/models.py
+++ bangazon/bangazonapi/models.py
@@ -1,8 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Users(models.Model):
-    first_name = models.CharField()
-    last_name = models.CharField()
-    shipping_address = models.CharField()
-    email = models.EmailField()
