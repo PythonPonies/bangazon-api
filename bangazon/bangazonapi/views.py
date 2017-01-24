7cdbd79b33099361be5358be45e6a36bd3bb1fa3
Nathan T. Baker <nathantbaker@gmail.com>
2017-01-24 15:09:39 -0600

    update gitignore and views for products and products-list

--- bangazon/bangazonapi/views.py
+++ bangazon/bangazonapi/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+class Products(generics.RetrieveAPIView):
+    queryset = Products.objects.all()
+    serializer_class = ProductsSerializer
+
+class Products_List(generics.ListAPIView):
+    queryset = Products.objects.all()
+    serializer_class = ProductsSerializer
