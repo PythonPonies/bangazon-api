82c22d83b5363121cd20985a6188fd69dddc38c3
Nathan T. Baker <nathantbaker@gmail.com>
2017-01-24 15:35:59 -0600

    add create and update methods to products serializer

--- bangazon/bangazonapi/serializers.py
+++ bangazon/bangazonapi/serializers.py
@@ -7,3 +7,13 @@ class ProductsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Products
         fields = ('id', 'title', 'description', 'price', 'quantity')
+
+    def create(self, validated_data):
+        return Comment(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get('description', instance.description)
+        instance.price = validated_data.get('price', instance.price)
+        instance.quantity = validated_data.get('quantity', instance.quantity)
+        return instance
