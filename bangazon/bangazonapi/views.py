3f2aa50e2fb6e547fcf274d6c4573baea597c96e
Joey Kirby <jkirbymusic@gmail.com>
2017-01-25 10:53:30 -0600

    Setting up User's  Model View Serializer Urls

--- bangazon/bangazonapi/views.py
+++ bangazon/bangazonapi/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from bangazonapi.serializers import UserSerializer
+from bangazonapi.models import User
+from rest_framework import generics
 
-# Create your views here.
+#user viewset makes sense of the request and produces the appropriate output
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
