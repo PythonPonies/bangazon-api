d58d49a706997ae8f339b58eb3b4d3b74857f235
ZoeLeBlanc <zoe.leblanc@vanderbilt.edu>
2017-03-03 12:42:17 -0600

    refactored models

--- bangazon/bangazonapi/admin.py
+++ bangazon/bangazonapi/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from bangazonapi.models import *
 
-admin.site.register(Customer)
-admin.site.register(Order)
-admin.site.register(Payment_Type)
-admin.site.register(Product)
-admin.site.register(Product_Category)
-admin.site.register(Product_On_Order)
+admin.site.register(customer_model.Customer)
+admin.site.register(order_model.Order)
+admin.site.register(paymenttype_model.PaymentType)
+admin.site.register(products_model.Product)
+admin.site.register(product_types_model.ProductTypes)
+admin.site.register(order_model.Product_On_Order)
